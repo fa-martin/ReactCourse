38aac6fb9f918ea47b8a052b56a6973ae602cbda
fa-martin <f.a.martin@hotmail.com>
2023-04-15 10:53:09 -0300

    Add some wrapped components and so on

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card"
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>
-        <div>{props.date.toLocaleString("en-US", { month: "long" })}</div>
-        <div>{props.date.toLocaleString("en-US", { day: "2-digit" })}</div>
-        <div>{props.date.getFullYear()}</div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2> {props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
