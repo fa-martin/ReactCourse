d4c4a56b51bd2d30093d23c8b66ee29014a9d728
fa-martin <f.a.martin@hotmail.com>
2023-04-16 09:52:39 -0300

    A typical example of state in react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
+import React, { useState } from 'react';
 
 function ExpenseItem(props) {
+  const [variableName, updateFunction] = useState(props.title);
+  
+  const clickHandler= ()=>{
+    updateFunction('new Value');
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2> {props.title}</h2>
+        <h2> {variableName}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>change title</button>
     </Card>
   );
 }
