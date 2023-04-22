08864f22d463583198ee4351fb75358a5f544e66
fa-martin <f.a.martin@hotmail.com>
2023-04-22 15:34:14 -0300

    dynamic list and features

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,19 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import React, { useState } from 'react';
+import React from 'react';
 
 function ExpenseItem(props) {
-  const [variableName, updateFunction] = useState(props.title);
+  const variableName= props.title;
   
-  const clickHandler= ()=>{
-    updateFunction('new Value');
-  };
+ 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2> {variableName}</h2>
         <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>change title</button>
+      </div>      
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,12 +3,12 @@ import "./ExpenseItem.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Expense(props) {
-  const [filterYear,setFilteredYear]=useState('2020');
+  const [filterYear, setFilteredYear] = useState("2020");
 
-  const filterExpense = selectedYear => {
+  const filterExpense = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
@@ -16,11 +16,15 @@ function Expense(props) {
   const ret = (
     <Card className="expenses">
       <ExpensesFilter Selected={filterYear} onChangeFilter={filterExpense} />
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      ></ExpenseItem>
+      {props.expenses.map((expense) => {
+        return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />)
+      })}
     </Card>
   );
   return ret;
